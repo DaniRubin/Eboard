0fdb05e6d3ca3037a5ad9f69b0ac86c862f57199
danirubin <sbh.ruchi@gmail.com>
2023-11-19 17:28:52 +0200

    Added About us page

--- src/components/Layout/SideNav.tsx
+++ src/components/Layout/SideNav.tsx
@@ -5,6 +5,7 @@ import prodLogo from "../../assets/ProductionLogo.svg";
 import telecommandLogo from "../../assets/TelecomandIcon.svg";
 import kashahLogo from "../../assets/KashahIcon.svg";
 import esufLogo from "../../assets/EsufIcon.svg";
+import infoLogo from "../../assets/InfoIcon.svg";
 import {useNavigate} from "react-router-dom";
 
 const SideNavStyle = styled.div`
@@ -20,7 +21,7 @@ const SideNavStyle = styled.div`
   background: #3A3A3A;
   //gap: 20px;
 `
-const SideNavOptionStyle = styled.div<{ isTitle: boolean, isSelected: boolean }>((props) => {
+const SideNavOptionStyle = styled.div<{ isTitle: boolean, isSelected: boolean, isLast: boolean }>((props) => {
     return `
     color: var(--light-grey, #E8E8E8);
       text-align: center;
@@ -33,12 +34,13 @@ const SideNavOptionStyle = styled.div<{ isTitle: boolean, isSelected: boolean }>
       padding: 20px;
       cursor: ${props.isTitle ? 'arrow' : 'pointer'};
       background-color: ${props.isSelected ? '#515151' : '#3A3A3A'};
+      margin-top: ${props.isLast ? 'auto' : 'null'};
       &:hover {
         background-color: ${props.isTitle ? '#3A3A3A' : '#BBB'};
       }
       div {
         margin-top: 8px;
-      }
+      };
 `
 })
 
@@ -53,23 +55,21 @@ const SideNav = () => {
 
     return <SideNavStyle>
         <SideNavOption logo={ebaordLogo} logoText={"satLogo"} OptionText={"Eboard"} isTitle={true} isSelected={false}
-                       onClick={() => {
-                       }}/>
-        <SideNavOption logo={prodLogo} logoText={"productionLogo"} OptionText={"Production"} isTitle={false}
-                       isSelected={window.location.pathname === '/'}
-                       onClick={() => navigate('/')}/>
-        <SideNavOption logo={satLogo} logoText={"satLogo"} OptionText={"Satellite"} isTitle={false}
+                       onClick={() => null}/>
+        <SideNavOption logo={prodLogo} logoText={"productionLogo"} OptionText={"Production"}
+                       isSelected={window.location.pathname === '/'} onClick={() => navigate('/')}/>
+        <SideNavOption logo={satLogo} logoText={"satLogo"} OptionText={"Satellite"}
                        isSelected={window.location.pathname.includes('satellite')}
                        onClick={() => navigate('/satellite/Sat1')}/>
-        <SideNavOption logo={telecommandLogo} logoText={"telecommand logo"} OptionText={"Telecommand"} isTitle={false}
+        <SideNavOption logo={telecommandLogo} logoText={"telecommand logo"} OptionText={"Telecommand"}
                        isSelected={window.location.pathname === '/telecommand'}
                        onClick={() => navigate('/telecommand')}/>
-        <SideNavOption logo={kashahLogo} logoText={"kashah logo"} OptionText={"Kashah"} isTitle={false}
-                       isSelected={window.location.pathname === '/kashah'}
-                       onClick={() => navigate('/kashah')}/>
-        <SideNavOption logo={esufLogo} logoText={"Esuf logo"} OptionText={"Esuf"} isTitle={false}
-                       isSelected={window.location.pathname === '/Esuf'}
-                       onClick={() => navigate('/Esuf')}/>
+        <SideNavOption logo={kashahLogo} logoText={"kashah logo"} OptionText={"Kashah"}
+                       isSelected={window.location.pathname === '/kashah'} onClick={() => navigate('/kashah')}/>
+        <SideNavOption logo={esufLogo} logoText={"Esuf logo"} OptionText={"Esuf"}
+                       isSelected={window.location.pathname === '/Esuf'} onClick={() => navigate('/Esuf')}/>
+        <SideNavOption logo={infoLogo} logoText={"Info logo"} OptionText={"About us"} isLast={true}
+                       isSelected={window.location.pathname === '/Info'} onClick={() => null}/>
     </SideNavStyle>
 
 }
@@ -80,11 +80,13 @@ type SideNavOptionProps = {
     OptionText: string,
     isTitle: boolean,
     onClick: () => void,
-    isSelected: boolean
+    isSelected: boolean,
+    isLast: boolean
 };
 
 const SideNavOption = (props: SideNavOptionProps) => (
-    <SideNavOptionStyle isTitle={props.isTitle} isSelected={props.isSelected} onClick={props.onClick}>
+    <SideNavOptionStyle isTitle={props.isTitle} isLast={props.isLast} isSelected={props.isSelected}
+                        onClick={props.onClick}>
         <NavImage src={props.logo} alt={props.logoText} isTitle={props.isTitle}/>
         <div>{props.OptionText}</div>
     </SideNavOptionStyle>
